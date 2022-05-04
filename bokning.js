00aa8e6e9090b23963bcf07ed738723f7d0c7b41
Ruofu Du <ruofu.du@skola.taby.se>
2022-05-04 14:31:14 +0200

    change

--- bokning.js
+++ bokning.js
@@ -1,7 +1,12 @@
 let boka = document.getElementsByClassName("boka");
-
+console.log(boka)
 for (let i = 0; i < boka.length; i++) {
-    boka[i].addEventListener("mouseover", function () {
-        boka[i].innerHTML = "boka";
+    boka[i].addEventListener("mouseover", function (e) {
+        e.target.innerHTML = "boka";
+    })
+}
+for (let i = 0; i < boka.length; i++) {
+    boka[i].addEventListener("mouseleave", function (e) {
+        e.target.innerHTML = "";
     })
 }
